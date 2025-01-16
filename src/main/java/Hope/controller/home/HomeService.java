ae725331eaebee9a29b15a87238c04d289418d88
CyrilTakam <127041301+JOJO182003@users.noreply.github.com>
2025-01-16 16:52:32 +0100

    Update HomeService.java

--- src/main/java/Hope/controller/home/HomeService.java
+++ src/main/java/Hope/controller/home/HomeService.java
@@ -32,4 +32,27 @@ public class HomeService {
     public List<Tool> searchData(String query) {
         return toolRepository.search(query);
     }
+
+    /*
+        public User getUser(String username) {
+        return userRepository.findUserByUsername(username).orElseThrow(() ->
+                new UserNotFoundException("Utilisateur non trouvé : " + username));
+    }
+
+    public List<Tool> getPreviewsData() {
+        List<Tool> tools = toolRepository.findAll();
+        if (tools.isEmpty()) {
+            throw new DataNotFoundException("Aucun outil trouvé.");
+        }
+        return tools;
+    }
+
+    public List<Tool> searchData(String query) {
+        List<Tool> results = toolRepository.search(query);
+        if (results.isEmpty()) {
+            throw new DataNotFoundException("Aucun résultat trouvé pour la requête : " + query);
+        }
+        return results;
+    }
+    */
 }
